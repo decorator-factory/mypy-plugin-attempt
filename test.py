c7f5bb51eadd054687719e3ba2a4adacad1b0cd0
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2022-01-12 09:55:05 +0300

    Add expected usage examples

--- test.py
+++ test.py
@@ -2,8 +2,36 @@ from attempt.things import struct
 
 UserInfo = struct(
     ("name", "string"),
+    ("age", "integer"),
     ("city", "string?"),
 )
+
+
+u1 = UserInfo()  # error
+
+u2 = UserInfo(  # ok
+    name="Bob",
+    age=42,
+    city="St. Petersburg",
+)
+
+u3 = UserInfo(  # ok
+    name="Bob",
+    age=42,
+    city=None,
+)
+
+u4 = UserInfo(  # error
+    name="Bob",
+    age=42,
+)
+
+u4 = UserInfo(  # error
+    name="Charlie",
+    age="old",
+    city=None
+)
+
 
 def f(u: UserInfo):
     reveal_type(u.name)
