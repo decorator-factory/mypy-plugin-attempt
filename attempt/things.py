6b3cdf5bd803403fe46c4242b9fe62612b478027
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2022-01-12 09:49:47 +0300

    Add implementation

--- attempt/things.py
+++ attempt/things.py
@@ -2,4 +2,16 @@ from typing import Any
 
 
 def struct(*fields) -> Any:
-    ...  # TODO:
+    fields_dict = dict(fields)
+
+    class Struct:
+        def __init__(self, **kwargs):
+            if extra_keys := kwargs.keys() - fields_dict.keys():
+                raise TypeError("Extra arguments to __init__", extra_keys)
+
+            if missing_keys := fields_dict.keys() - kwargs.keys():
+                raise TypeError("Missing arguments to __init__", missing_keys)
+
+            self.__dict__.update(kwargs)
+
+    return Struct
